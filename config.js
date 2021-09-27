1c3093668467089ca23e3a1438881e3303851538
Augfusin <91377855+Augfusin@users.noreply.github.com>
2021-09-27 19:34:07 +0900

    udate

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introTitle: '🖐🖐🖐 Chào Bé Yêu! 🖐🖐🖐',
+    introDesc: `Anh có điều muốn hỏi Bé nè, Bé  nhớ phải trả lời thật lòng he <3`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
+    title: 'Anh biết Bé thương Anh nhiều lắm phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em Thương Anh đó nha :> ',
+    btnYes: 'Có, Bé thật sự thương rất nhiều nhiều nhiều anh 💖💖💖',
     btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    question:'Hãy nói cho Anh nghe  vì sao Bé thương anh niều😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
     reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    mess: 'Anh biết mà, Anh cũng thương bé nhiều lắm lắm 🥰. I Love you forever 😘😘',
     messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    btnAccept: 'Dạ Anh 💖',
+    messLink: 'https://www.messenger.com/t/100003233111342/'
 }
